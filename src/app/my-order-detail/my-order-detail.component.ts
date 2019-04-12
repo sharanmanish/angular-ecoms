c91688835b290993be490e152d58c875b3b8c563
sharanmanish <manishsharan@live.com>
2019-04-12 16:58:22 +0530

    all done

--- src/app/my-order-detail/my-order-detail.component.ts
+++ src/app/my-order-detail/my-order-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from '../rest-api.service';
+import { DataService } from '../data.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-order-detail',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyOrderDetailComponent implements OnInit {
 
-  constructor() { }
+  order: any;
+
+  constructor(
+    private data: DataService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private rest: RestApiService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(res => {
+      this.rest.get(`http://localhost:3030/api/orders/${res['id']}`)
+        .then(data => {
+          data['success']
+            ? (this.order = data['order'])
+            : this.router.navigate(['/']);
+        }).catch(error => this.data.error(error['message']));
+    })
   }
 
 }
